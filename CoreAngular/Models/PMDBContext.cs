d3c54e43d6811b1a2b5455b7363b8bdd110bc973
scsl2 <scsl2@SCSL30>
2024-08-21 15:38:16 +0600

    Add project files.

---
+++ CoreAngular/Models/PMDBContext.cs
@@ -0,0 +1,20 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreAngular.Models
+{
+    public class PMDBContext : DbContext
+    {
+        public PMDBContext(DbContextOptions<PMDBContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>()
+                .Property(p => p.Price)
+                .HasColumnType("decimal(18,2)"); // You can adjust the precision and scale as needed
+        }
+
+    }
+}
---
+++ CoreAngular/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace CoreAngular.Models
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string ProName { get; set; }
+        public string Type { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
